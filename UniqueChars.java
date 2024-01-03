045fc54b56f51b3ca46a0b90b67394653fb5d566
Omry Maor <omry.maor@runi.ac.il>
2024-01-03 14:28:48 +0200

    Finished UniqueChars.

--- UniqueChars.java
+++ UniqueChars.java
@@ -11,7 +11,19 @@ public class UniqueChars {
      * unless they are space characters.
      */
     public static String uniqueChars(String s) {
-        // Replace the following statement with your code
-        return null;
+        String uniqueString = "";
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == ' ') { // if char is space, add and go on
+                uniqueString += s.charAt(i);
+            }
+            if (uniqueString.contains(String.valueOf(s.charAt(i)))) {
+                // checks if a letter is already inside the string or not. If yes, do nothing
+            } else {
+                uniqueString += s.charAt(i); // If not, meaning that it's unique - add it
+            }
+        }
+
+        return uniqueString;
     }
 }
