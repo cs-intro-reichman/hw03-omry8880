733fdb389640b4d9ebe284fcda16a2fae2a304e9
Omry Maor <omry.maor@runi.ac.il>
2024-01-04 08:47:05 +0200

    Finished Calendar0

--- Calendar0.java
+++ Calendar0.java
@@ -22,13 +22,19 @@ public class Calendar0 {
 
 	// Tests the nDaysInMonth function.
 	private static void nDaysInMonthTest(int year) {
-		// Replace this comment with your code
+		for (int i = 0; i < 12; i++) {
+			System.out.println("Month " + (i+1) + " has " + nDaysInMonth(i+1, year) + " days");
+		}
 	}
 
 	// Returns true if the given year is a leap year, false otherwise.
 	public static boolean isLeapYear(int year) {
-	    // Replace the following statement with your code
-		return false;
+		// Checks if the year is divisible by 400
+		boolean leapYear = ((year % 400) == 0);
+		// Then checks if the year is divisible by 4 but not by 100
+		leapYear = leapYear || (((year % 4) == 0) && ((year % 100) != 0));
+
+		return leapYear;
 	}
 	 
 	// Returns the number of days in the given month and year.
@@ -36,8 +42,55 @@ public class Calendar0 {
 	// February has 28 days in a common year, and 29 days in a leap year.
 	// All the other months have 31 days.
 	public static int nDaysInMonth(int month, int year) {
-		// Replace the following statement with your code
-		return 0;
+		/*
+		 * November, April, June, and September are the months with 30 days , 
+		 * February has 28 days (29 days in the leap year) and all the remaining months have 31 days.
+		 */
+		 int days = 0;
+
+		switch (month) {
+			case 1:
+				days = 31;
+				break;
+			case 2:
+				if (isLeapYear(year)) {
+					days = 29;
+					break;
+				}
+				days = 28;
+					break;
+							case 3:
+				days = 31;
+					break;
+			case 4:
+				days = 30;
+					break;
+							case 5:
+				days = 31;
+					break;
+			case 6:
+				days = 30;
+					break;
+							case 7:
+				days = 31;
+					break;
+							case 8:
+				days = 31;
+					break;
+			case 9:
+				days = 30;
+					break;
+							case 10:
+				days = 31;
+					break;
+			case 11:
+				days = 30;
+					break;
+			case 12:
+				days = 31;
+					break;
+		}
+		return days;
 	}
 }
 
