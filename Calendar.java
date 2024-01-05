09508515724c51408695015b6644d7f24a9f3bbf
Omry Maor <omry.maor@runi.ac.il>
2024-01-05 21:39:14 +0200

    Finished Calendar

--- Calendar.java
+++ Calendar.java
@@ -1,38 +1,28 @@
 /** 
  * Prints the calendars of all the years in the 20th century.
  */
-public class Calendar1 {	
-    // Starting the calendar on 1/1/1900
+public class Calendar {	
+    static int year;
+	static int inputYear;
 	static int dayOfMonth = 1;   
 	static int month = 1;
-	static int year = 1900;
 	static int dayOfWeek = 2;     // 1.1.1900 was a Monday 
+	static int totalDays = 0;
 	static int nDaysInMonth = 31; // Number of days in January
 	static int specialSunday = 0; // Number of sundays that fall on the 1st of each month
 	
-	/** 
-	 * Prints the calendars of all the years in the 20th century. Also prints the  
-	 * number of Sundays that occured on the first day of the month during this period.
-	 */
 	public static void main(String args[]) {
-		// Advances the date and the day-of-the-week from 1/1/1900 till 31/12/1999, inclusive.
-	    // Prints each date dd/mm/yyyy in a separate line. If the day is a Sunday, prints "Sunday".
-	    // The following variable, used for debugging purposes, counts how many days were advanced so far.
-	    // int debugDaysCounter = 0; 
-	    //// Write the necessary initialization code, and replace the condition
-	    //// of the while loop with the necessary condition 
-	 	while (year < 2000) {
-	 		System.out.println(dayOfMonth + "/" + month + "/" + year + (dayOfWeek == 1 ? " Sunday" : "")); // Monday = 2, 		
-	 		advance(); 
-	 		// debugDaysCounter++;
-	 		//// If you want to stop the loop after n days, replace the condition of the
-	 		//// if statement with the condition (debugDaysCounter == n)
-	 		// if (debugDaysCounter == 1000) { 
-	 		//	break;
-	 		// }
+		inputYear = Integer.parseInt(args[0]);
+		year = 0;
+
+	 	while (year < inputYear) {
+	 		advanceDaysOnly(); 
         }
-		System.out.println("During the 20th century, " + specialSunday + " Sundays fell on the first day of the month");
-	 	//// Write the necessary ending code here
+
+		while (year < inputYear + 1) {
+	 		System.out.println(dayOfMonth + "/" + month + "/" + inputYear + (dayOfWeek == 1 ? " Sunday" : ""));		
+	 		advance(); 
+		}
 	 }
 	
 	 // Advances the date (day, month, year) and the day-of-the-week.
@@ -46,19 +36,30 @@ public class Calendar1 {
 			dayOfWeek = 1;
 		}
 
-		if (dayOfMonth - 1 == nDaysInMonth(month, year)) {
+		if (dayOfMonth - 1 == nDaysInMonth(month, inputYear)) {
 			dayOfMonth = 1;
 			if (month == 12) {
-				year = year + 1;
 				month = 0;
+				year++;
 			}
 			month = month + 1;
 		}
+	 }
 
-		if (dayOfWeek == 1 && dayOfMonth == 1) {
-			specialSunday++;
+	 private static void advanceDaysOnly() {
+		dayOfWeek = dayOfWeek + 1;
+		totalDays++;
+
+		if (dayOfWeek == 8) {
+			dayOfWeek = 1;
 		}
-	 } 
+
+		if (isLeapYear(year) && totalDays == 364 || !isLeapYear(year) && totalDays == 365) {
+			year++;
+			totalDays = 0;
+		}
+		
+	 }
 		 
     // Returns true if the given year is a leap year, false otherwise.
 	private static boolean isLeapYear(int year) {
